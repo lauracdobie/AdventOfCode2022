017085299cdeb42dd128fdb1e4f5a0618a15957a
Laura Dobie <moving.toyshop@gmail.com>
2022-12-02 20:43:23 +0000

    Day 2 extension working with small dataset

--- day2.py
+++ day2.py
@@ -2,7 +2,6 @@ data = []
 fileobj=open("day2_input.txt")
 data=fileobj.read().split('\n')
 
-n = 2
 list_of_lists = []
 
 for item in data:
@@ -11,7 +10,6 @@ for item in data:
     list.append(item[2])
     list_of_lists.append(list)
 
-print(list_of_lists)
  
 def calculate_score(p1, p2):
     score = 0
@@ -42,6 +40,38 @@ def calculate_score(p1, p2):
         score += 3
        
     return score
+
+def calculate_new_score(p1, p2):
+    score = 0
+       
+    if p2 == "X" and p1 == "A":
+        score += 3
+    
+    if p2 == "X" and p1 == "B":
+        score += 1
+    
+    if p2 == "X" and p1 == "C":
+        score += 2
+       
+    if p2 == "Y" and p1 == "A":
+        score += 4
+   
+    if p2 == "Y" and p1 == "B":
+        score += 5
+
+    if p2 == "Y" and p1 == "C":
+        score += 6
+    
+    if p2 == "Z" and p1 == "A":
+        score += 8 
+    
+    if p2 == "Z" and p1 == "B":
+        score += 9
+   
+    if p2 == "Z" and p1 == "C":
+        score += 7
+       
+    return score
  
 final_score = 0
  
@@ -50,3 +80,16 @@ for list in list_of_lists:
     final_score += result
    
 print(final_score)
+
+test_data = ["A", "Y", "B", "X", "C", "Z"]
+n = 2
+
+test_list_of_lists = [test_data[i * n:(i + 1) * n] for i in range((len(test_data) + n - 1) // n )]
+
+new_final_score = 0
+
+for list in test_list_of_lists:
+    result = calculate_new_score(list[0], list[1])
+    new_final_score += result
+   
+print(new_final_score)
