1151e6600ccf693e55705f3a0b9ea8074243238c
Laura Dobie <moving.toyshop@gmail.com>
2022-12-04 15:30:43 +0000

    Day 4 first part complete

--- day4.py
+++ day4.py
@@ -7,34 +7,29 @@ test_data = ["2-4,6-8",
 "6-6,4-6",
 "2-6,4-8"]
 
-for item in test_data:
+data = []
+fileobj=open("day4_input.txt")
+data=fileobj.read().split('\n')
+
+for item in data:
     lists = item.split(",")
 
-    list_1 = lists[0]
-    list_2 = lists[1]
+    list_1 = lists[0].split("-")
+    list_2 = lists[1].split("-")
 
     new_list_1 = []
     new_list_2 = []
 
-    for i in range(int(list_1[0]), int(list_1[2]) +1):
+    for i in range(int(list_1[0]), int(list_1[1]) +1):
         new_list_1.append(i)
 
-    print(new_list_1)
-
-    for i in range(int(list_2[0]), int(list_2[2]) +1):
+    for i in range(int(list_2[0]), int(list_2[1]) +1):
         new_list_2.append(i)
 
-    print(new_list_2)
-
     if new_list_1[len(new_list_1) - 1] >= new_list_2[len(new_list_2) -1] and new_list_1[0] <= new_list_2[0]:
-        print("One list contains the other")
         total += 1
 
     elif new_list_2[len(new_list_2) - 1] >= new_list_1[len(new_list_1) -1] and new_list_2[0] <= new_list_1[0]:
-        print("One list contains the other - second statement")
         total += 1
-
-    else:
-        print("One list does not contain the other")
 
 print("End total: " + str(total))
