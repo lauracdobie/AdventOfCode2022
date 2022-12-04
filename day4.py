2bd20d918592ea68587cff40b8d8f45aa4774c93
Laura Dobie <moving.toyshop@gmail.com>
2022-12-04 15:14:28 +0000

    Logic working with small test dataset

--- day4.py
+++ day4.py
@@ -1,33 +1,40 @@
 total = 0
 
-string = "2-8,3-7"
-lists = string.split(",")
+test_data = ["2-4,6-8",
+"2-3,4-5",
+"5-7,7-9",
+"2-8,3-7",
+"6-6,4-6",
+"2-6,4-8"]
 
-list_1 = lists[0]
-list_2 = lists[1]
+for item in test_data:
+    lists = item.split(",")
 
-new_list_1 = []
-new_list_2 = []
+    list_1 = lists[0]
+    list_2 = lists[1]
 
-for i in range(int(list_1[0]), int(list_1[2]) +1):
-    new_list_1.append(i)
+    new_list_1 = []
+    new_list_2 = []
 
-print(new_list_1)
+    for i in range(int(list_1[0]), int(list_1[2]) +1):
+        new_list_1.append(i)
 
-for i in range(int(list_2[0]), int(list_2[2]) +1):
-    new_list_2.append(i)
+    print(new_list_1)
 
-print(new_list_2)
+    for i in range(int(list_2[0]), int(list_2[2]) +1):
+        new_list_2.append(i)
 
-if new_list_1[len(new_list_1) - 1] >= new_list_2[len(new_list_2) -1] and new_list_1[0] <= new_list_2[0]:
-    print("One list contains the other")
-    total += 1
+    print(new_list_2)
 
-elif new_list_2[len(new_list_2) - 1] >= new_list_1[len(new_list_1) -1] and new_list_2[0] <= new_list_1[0]:
-    print("One list contains the other")
-    total += 1
+    if new_list_1[len(new_list_1) - 1] >= new_list_2[len(new_list_2) -1] and new_list_1[0] <= new_list_2[0]:
+        print("One list contains the other")
+        total += 1
 
-else:
-    print("One list does not contain the other")
+    elif new_list_2[len(new_list_2) - 1] >= new_list_1[len(new_list_1) -1] and new_list_2[0] <= new_list_1[0]:
+        print("One list contains the other - second statement")
+        total += 1
 
-print(total)
+    else:
+        print("One list does not contain the other")
+
+print("End total: " + str(total))
