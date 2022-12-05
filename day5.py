c5c93bc8c4cc586613dcd1315c58733c01f5fac3
Laura Dobie <moving.toyshop@gmail.com>
2022-12-05 23:05:06 +0000

    Day 5 extension complete

--- day5.py
+++ day5.py
@@ -20,17 +20,23 @@ nine = ["M", "C", "L", "G", "V", "R", "T"]
 lists = {"1": one, "2": two, "3": three, "4": four, "5": five, "6": six, "7": seven, "8": eight, "9": nine}
  
 def move_crates(number_to_move, from_list, to_list):
+  intermediate_list = []
   while number_to_move > 0:
     item = from_list[len(from_list) - 1]
-    to_list.append(item)
+    intermediate_list.append(item)
     from_list.pop(len(from_list) - 1)
     number_to_move -= 1
+  
+  for item in list(reversed(intermediate_list)):
+    to_list.append(item)
  
 for move in moves:
   move_list = move.split(" ")
   move_crates(int(move_list[1]), lists[move_list[3]], lists[move_list[5]])
 
 result = one[len(one) - 1] + two[len(two) -1] + three[len(three) - 1] + four[len(four) - 1] + five[len(five) - 1] + six[len(six) - 1] + seven[len(seven) - 1] + eight[len(eight) - 1] + nine[len(nine) - 1]
+
 print(one)
 print(nine)
+
 print(result)
