4c02c1eefd897bc4573665de56b72cb8f9c4dce2
Laura Dobie <moving.toyshop@gmail.com>
2022-12-06 21:38:49 +0000

    Code working with small dataset

---
+++ day6.py
@@ -0,0 +1,37 @@
+marker_list = []
+ 
+string = "mjqjpqmgbljsphdztnvjfqwrcgsmlb" #Result should be 7
+string1 = "bvwbjplbgvbhsrlpgdmjqwftvncz" #Result should be 5
+string2 = "nppdvjthqldpwncqszvftbrmjlhg" #Result should be 6
+string3 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" #Result should be 10
+string4 = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" #Result should be 11
+ 
+list = []
+ 
+new_list = []
+ 
+for char in string:
+  list.append(char)
+ 
+start_index = 0
+end_index = 4
+
+block_to_check = list[start_index:end_index]
+
+print("Block to check: " + str(block_to_check))
+
+found = False
+
+while found == False:
+    print("New block to check: " + str(block_to_check))
+    for letter in block_to_check:
+        if letter not in marker_list:
+            marker_list.append(letter)
+        if len(marker_list) == 4 :
+            print("Result is: " + str(end_index))
+            found = True
+    marker_list = []
+    start_index += 1
+    end_index += 1
+    block_to_check = list[start_index:end_index]
+ 
